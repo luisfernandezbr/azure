0e6f3ab5a041aae6b00ef98c8aadb13e02f98c63
Matthew Congrove <mcongrove@pinpoint.com>
2020-11-04 11:44:51 -0600

    Update integration logos

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
 			tags: ['Source Code Management', 'Issue Management'],
 			installed: false,
 			refType: 'azure',
-			icon: `data:image/svg+xml,%3Csvg id='f4337506-5d95-4e80-b7ca-68498c6e008e' data-name='fluent_icons' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='18' height='18' viewBox='0 0 18 18'%3E%3Cdefs%3E%3ClinearGradient id='ba420277-700e-42cc-9de9-5388a5c16e54' x1='9' y1='16.97' x2='9' y2='1.03' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%230078d4'/%3E%3Cstop offset='0.16' stop-color='%231380da'/%3E%3Cstop offset='0.53' stop-color='%233c91e5'/%3E%3Cstop offset='0.82' stop-color='%23559cec'/%3E%3Cstop offset='1' stop-color='%235ea0ef'/%3E%3C/linearGradient%3E%3C/defs%3E%3Ctitle%3EIcon-devops-261%3C/title%3E%3Cpath id='a91f0ca4-8fb7-4019-9c09-0a52e2c05754' data-name='Path 1' d='M17,4v9.74l-4,3.28-6.2-2.26V17L3.29,12.41l10.23.8V4.44Zm-3.41.49L7.85,1V3.29L2.58,4.84,1,6.87v4.61l2.26,1V6.57Z' fill='url(%23ba420277-700e-42cc-9de9-5388a5c16e54)'/%3E%3C/svg%3E`,
+			icon: 'https://pinpoint.com/images/integrations/Azure.svg',
 			publisher: {
 				name: 'Pinpoint',
-				avatar: 'https://avatars0.githubusercontent.com/u/24400526?s=200&v=4',
+				avatar: 'https://pinpoint.com/logo/logomark/blue.png',
 				url: 'https://pinpoint.com'
 			},
 			uiURL: window.location.href
